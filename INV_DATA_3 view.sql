b13ecdec10f2c31162456835220667a4fdef4a5c
Chris Luehmann <crluehmann@gmail.com>
2015-09-04 13:44:00 -0400

    bug fixes

--- INV_DATA_3 view.sql
+++ INV_DATA_3 view.sql
@@ -4,10 +4,10 @@ SUM(IN06) AS INSTK_EAST,
 SUM(PO06) AS PO_EAST,
 SUM(OT06) AS OTW_EAST,
 SUM(OR06) AS ORD_EAST,
-SUM(AV06) AS AVAIL_ALL
+SUM(AV06) AS AVAIL_ALL,
                           (SELECT     TOP (1) CLASS_CD
                             FROM          omsdata2.dbo.inv
                             WHERE      (PROD_CD = omsdata2.dbo.HL_SUM_INV_DATA_2_WHS_06.PROD_CD)) AS CLASSCODE
-FROM         omsdata2.dbo.HL_SUM_INV_DATA_2
+FROM         omsdata2.dbo.HL_SUM_INV_DATA_2_WHS_06
 GROUP BY PROD_CD
 ORDER BY PROD_CD
