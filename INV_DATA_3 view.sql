c53255b6d9fa20bd1fcda1b8e20c881716db1952
Chris Luehmann <crluehmann@gmail.com>
2015-09-04 13:34:20 -0400

    damn comma

--- INV_DATA_3 view.sql
+++ INV_DATA_3 view.sql
@@ -4,7 +4,7 @@ SUM(IN06) AS INSTK_EAST,
 SUM(PO06) AS PO_EAST,
 SUM(OT06) AS OTW_EAST,
 SUM(OR06) AS ORD_EAST,
-SUM(AV06) AS AVAIL_ALL,
+SUM(AV06) AS AVAIL_ALL
                           (SELECT     TOP (1) CLASS_CD
                             FROM          omsdata2.dbo.inv
                             WHERE      (PROD_CD = omsdata2.dbo.HL_SUM_INV_DATA_2.PROD_CD)) AS CLASSCODE
